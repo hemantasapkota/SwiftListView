7ebfb5eaef6fa894a2404d073b9ea47869ebbf32
Hemanta Sapkota <laex.pearl@gmail.com>
2016-10-19 14:03:07 +1100

    Update example

--- Example/SwiftListViewExample/SwiftListViewExample/ViewController.swift
+++ Example/SwiftListViewExample/SwiftListViewExample/ViewController.swift
@@ -8,10 +8,15 @@
 
 import UIKit
 import SwiftListView
+import GCDTimer
 
 class ViewController: UIViewController {
     
-    @IBOutlet var button: UIButton!
+    @IBOutlet var basicListViewBtn: UIButton!
+    
+    @IBOutlet var basicListViewBtn2: UIButton!
+    
+    var selectedItem = "Himalayan Cat"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,9 +28,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func onButtonClicked() {
-        let listView = BasicListView(viewTitle: "Hello", highlighted: "Hi")
-        listView.Items = ["Item1", "Item2", "Item3", "Item4"]
+    @IBAction func onBasicListViewBtnClicked() {
+        let listView = BasicListView(viewTitle: "Cat Breeds", highlighted: selectedItem)
+        
+        listView.Items = ["American Bobtail", "American Curl", "American Shorthair", "Himalayan Cat"]
+        
+        listView.onSelection = { selected in
+            self.basicListViewBtn.titleLabel?.text = selected
+            self.selectedItem = selected
+        }
+        
+        listView.show()
+    }
+    
+    @IBAction func onBasicListViewBtn2Clicked() {
+        let listView = BasicListView(viewTitle: "", highlighted: "")
+        
+        listView.ShowProgress = true
+        
+        listView.ItemsLoader = { (completion) in
+            GCDTimer.delay(0.8, block: {
+                listView.ShowProgress = false
+                completion!(["Dog Breed", "Labrador Retriever", "German Shepherd"])
+            })
+        }
+        
         listView.show()
     }
 
