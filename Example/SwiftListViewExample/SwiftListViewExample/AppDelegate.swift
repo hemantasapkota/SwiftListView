7d048e2e03211d10ddbce8a380027d4e29393878
Hemanta Sapkota <laex.pearl@gmail.com>
2016-11-15 17:31:45 +1100

    Update example

--- Example/SwiftListViewExample/SwiftListViewExample/AppDelegate.swift
+++ Example/SwiftListViewExample/SwiftListViewExample/AppDelegate.swift
@@ -16,8 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
         
         OpenSans.registerFonts()
+        
+        let viewController = ViewController()
+        self.window?.rootViewController = viewController
+        self.window?.makeKeyAndVisible()
         
         // Override point for customization after application launch.
         return true
--- Example/SwiftListViewExample/SwiftListViewExample/ViewController.swift
+++ Example/SwiftListViewExample/SwiftListViewExample/ViewController.swift
@@ -9,18 +9,17 @@
 import UIKit
 import SwiftListView
 import GCDTimer
+import SnapKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var basicListViewBtn: UIButton!
-    
-    @IBOutlet var basicListViewBtn2: UIButton!
-    
     var selectedItem = "Himalayan Cat"
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view = View()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,33 +27,90 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func onBasicListViewBtnClicked() {
-        let listView = BasicListView(viewTitle: "Cat Breeds", highlighted: selectedItem)
-        
-        listView.Items = ["American Bobtail", "American Curl", "American Shorthair", "Himalayan Cat"]
-        
-        listView.onSelection = { selected in
-            self.basicListViewBtn.titleLabel?.text = selected
-            self.selectedItem = selected
-        }
-        
-        listView.show()
-    }
-    
     @IBAction func onBasicListViewBtn2Clicked() {
         let listView = BasicListView(viewTitle: "", highlighted: "")
-        
         listView.ShowProgress = true
-        
         listView.ItemsLoader = { (completion) in
             GCDTimer.delay(0.8, block: {
                 listView.ShowProgress = false
                 completion!(["Dog Breed", "Labrador Retriever", "German Shepherd"])
             })
         }
-        
         listView.show()
     }
 
+}
+
+class View : UIView {
+    
+    var showCatBreed: GroupFilterButton!
+    
+    var showDogBreed: GroupFilterButton!
+    
+    init() {
+        super.init(frame: UIScreen.main.bounds)
+        
+        self.backgroundColor = UIColor.white
+        
+        // Cat Breed
+        var selectedCat = ""
+        
+        showCatBreed = GroupFilterButton()
+        showCatBreed.SelectedText = "Choose Cat Breed"
+        showCatBreed.onSelection = { selected in
+            let listView = BasicListView(viewTitle: "Cat Breeds", highlighted: selectedCat)
+            
+            listView.Items = ["American Bobtail", "American Curl", "American Shorthair", "Himalayan Cat"]
+            listView.onSelection = { selected in
+                self.showCatBreed.SelectedText = selected
+                selectedCat = selected
+            }
+            
+            listView.show()
+            
+        }
+        addSubview(showCatBreed)
+        showCatBreed.snp.makeConstraints { (make) in
+            make.centerX.equalTo(self.snp.centerX)
+            make.centerY.equalTo(self.snp.centerY).offset(-60)
+            make.width.equalTo(200)
+            make.height.equalTo(60)
+        }
+        
+        // Dog Breed
+        var selectedDog = ""
+        
+        showDogBreed = GroupFilterButton()
+        showDogBreed.SelectedText = "Choose dog breed"
+        showDogBreed.onSelection = { selected in
+            let listView = BasicListView(viewTitle: "", highlighted: selectedDog)
+            listView.ShowProgress = true
+            listView.onSelection = { selected in
+                self.showDogBreed.SelectedText = selected
+                selectedDog = selected
+            }
+            listView.ItemsLoader = { (completion) in
+                GCDTimer.delay(0.8, block: {
+                    listView.ShowProgress = false
+                    completion!(["Dog Breed", "Labrador Retriever", "German Shepherd"])
+                })
+            }
+            listView.show()
+        }
+        addSubview(showDogBreed)
+        showDogBreed.snp.makeConstraints { (make) in
+            make.top.greaterThanOrEqualTo(showCatBreed.snp.bottom).offset(20)
+            make.centerX.equalTo(self.snp.centerX)
+            
+            make.width.equalTo(200)
+            make.height.equalTo(60)
+        }
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
 
